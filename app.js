3cd6623ff0d59cdf5820dbf629b5724cebfd3428
QweetGnarer <giga.nemsitsveridze2002@gmail.com>
2020-01-06 01:04:24 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function message() {
+    let name = "Giga";
+    alert("you clicked me!");
+    console.log(name);
+}
+// es ras unda werdes?
+
+function addThisTwoNumber(firstNumber, secondNumber) { // gadmoscem 10-s da 20-s ori ricxvi unda gadasce -- rafer :D
+    let number = firstNumber + secondNumber; // magat shekrebs; krebs 10-s da 20-s
+    log(number); // amas gadascems haaaaA? bechdavs 30-s ;D izams axla? navxot
+}
+function log(param) { // es miighebs
+    console.log(param); // es dabechdavs :D
+}
